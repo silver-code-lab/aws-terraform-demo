d1207ae2c7eeab46b3ed8ffd25bab5f94186158f
Artium Shevchenko <artium7770@gmail.com>
2025-07-26 00:26:01 +0300

    instances.tf

--- instances.tf
+++ instances.tf
@@ -1,9 +1,11 @@
-resource "aws_security_group" "web_sg" {
-  name        = "web-sg"
-  description = "Allow SSH and port 3000"
+
+resource "aws_security_group" "instance_sg" {
+  name        = "allow_ssh_and_app"
+  description = "Allow SSH and app port"
   vpc_id      = aws_vpc.main.id
 
   ingress {
+    description = "Allow SSH"
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
@@ -11,6 +13,7 @@ resource "aws_security_group" "web_sg" {
   }
 
   ingress {
+    description = "App Port"
     from_port   = 3000
     to_port     = 3000
     protocol    = "tcp"
@@ -23,36 +26,54 @@ resource "aws_security_group" "web_sg" {
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
+  tags = {
+    Name = "instance-sg"
+  }
 }
 
 resource "aws_instance" "web1" {
-  ami                         = "ami-0f918f7e67a3323f0"
+  ami                         = "ami-053b12d3152c0cc71" # Ubuntu 22.04 (Mumbai)
   instance_type               = "t3a.micro"
-  subnet_id                   = aws_subnet.subnet1.id
+  subnet_id                   = aws_subnet.public_1.id
   key_name                    = "EC2_SSH_KEY"
   associate_public_ip_address = true
-  vpc_security_group_ids      = [aws_security_group.web_sg.id]
+  vpc_security_group_ids      = [aws_security_group.instance_sg.id]
 
   user_data = <<-EOF
-    #!/bin/bash
-    apt-get update
-    apt-get install -y docker.io
-    docker run -d -p 3000:3000 ghcr.io/eldadfux/hostname-docker
-  EOF
+              #!/bin/bash
+              apt-get update -y
+              apt-get install -y docker.io
+              usermod -aG docker ubuntu
+              systemctl start docker
+              systemctl enable docker
+              docker run -d -p 3000:3000 adongy/hostname-docker
+              EOF
+
+  tags = {
+    Name = "web-server-1"
+  }
 }
 
 resource "aws_instance" "web2" {
-  ami                         = "ami-0f918f7e67a3323f0"
+  ami                         = "ami-053b12d3152c0cc71"
   instance_type               = "t3a.micro"
-  subnet_id                   = aws_subnet.subnet2.id
+  subnet_id                   = aws_subnet.public_2.id
   key_name                    = "EC2_SSH_KEY"
   associate_public_ip_address = true
-  vpc_security_group_ids      = [aws_security_group.web_sg.id]
+  vpc_security_group_ids      = [aws_security_group.instance_sg.id]
 
   user_data = <<-EOF
-    #!/bin/bash
-    apt-get update
-    apt-get install -y docker.io
-    docker run -d -p 3000:3000 ghcr.io/eldadfux/hostname-docker
-  EOF
+              #!/bin/bash
+              apt-get update -y
+              apt-get install -y docker.io
+              usermod -aG docker ubuntu
+              systemctl start docker
+              systemctl enable docker
+              docker run -d -p 3000:3000 adongy/hostname-docker
+              EOF
+
+  tags = {
+    Name = "web-server-2"
+  }
 }
