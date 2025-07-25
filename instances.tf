0dcc85466d9f776ee2b36874843364c1aa1aa301
Artium Shevchenko <artium7770@gmail.com>
2025-07-26 00:32:47 +0300

    instances.tf

--- instances.tf
+++ instances.tf
@@ -32,8 +32,33 @@ resource "aws_security_group" "instance_sg" {
   }
 }
 
+resource "aws_security_group" "alb_sg" {
+  name        = "alb_sg"
+  description = "Security group for ALB"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    description = "Allow HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "alb-security-group"
+  }
+}
+
 resource "aws_instance" "web1" {
-  ami                         = "ami-053b12d3152c0cc71" # Ubuntu 22.04 (Mumbai)
+  ami                         = "ami-053b12d3152c0cc71"
   instance_type               = "t3a.micro"
   subnet_id                   = aws_subnet.public_1.id
   key_name                    = "EC2_SSH_KEY"
@@ -76,4 +101,60 @@ resource "aws_instance" "web2" {
   tags = {
     Name = "web-server-2"
   }
+}
+
+resource "aws_lb" "web" {
+  name               = "web-alb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.alb_sg.id]
+  subnets            = [aws_subnet.public_1.id, aws_subnet.public_2.id]
+
+  tags = {
+    Name = "web-alb"
+  }
+}
+
+resource "aws_lb_target_group" "web" {
+  name     = "web-target-group"
+  port     = 3000
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.main.id
+
+  health_check {
+    path                = "/"
+    protocol            = "HTTP"
+    matcher             = "200"
+    interval            = 30
+    timeout             = 5
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+  }
+
+  tags = {
+    Name = "web-target-group"
+  }
+}
+
+resource "aws_lb_listener" "web" {
+  load_balancer_arn = aws_lb.web.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.web.arn
+  }
+}
+
+resource "aws_lb_target_group_attachment" "web1" {
+  target_group_arn = aws_lb_target_group.web.arn
+  target_id        = aws_instance.web1.id
+  port             = 3000
+}
+
+resource "aws_lb_target_group_attachment" "web2" {
+  target_group_arn = aws_lb_target_group.web.arn
+  target_id        = aws_instance.web2.id
+  port             = 3000
 }
